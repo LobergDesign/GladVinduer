ee5ceb792b7a5bfbc37e471613851680cd2b992f
LobergDesign <jeanloberg@hotmail.dk>
2017-11-12 16:27:39 +0100

    first commit
    first

---
+++ GladVinduer/Controllers/MasterController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace GladVinduer.Controllers
+{
+    public class MasterController : RenderMvcController
+    {
+        // GET: Master
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
